8abc5359365cb8612a8f8d0a3c3e31420017f5a8
eduar-vivas <vivad796@gmail.com>
2025-05-23 21:25:28 -0600

    commit final

--- Developer_page_7SA/Proyecto-gif-Eduardo-creando/Cpta src/main.jsx
+++ Developer_page_7SA/Proyecto-gif-Eduardo-creando/Cpta src/main.jsx
@@ -1,76 +1,55 @@
-// Importamos la biblioteca de React
+ // Importamos la biblioteca de React
 import React from 'react'
 // Importamos la biblioteca ReactDOM, que proporciona métodos específicos del DOM para
-React
+//React
 import ReactDOM from 'react-dom/client'
 // Importamos un componente llamado ProyectoGif desde otro archivo en el mismo directorio
 import { ProyectoGif } from './ProyectoGif'
 //Importamos los estilos que se encuentran en src. ¿Qué significaba "./"?
 import './styles.css'
 
-
-//funcion tra
-function (a)
-{
-    return a + 100;
-}
-
-(a) => a + 100;
-
-ReactDOM.createRoot(document.getElementById('root')).render
-(
-// React.StrictMode es un componente especial de React que comprueba si tu aplicación
-//tiene problemas potenciales durante el desarrollo
-    <React.StrictMode>
-        {/* Aquí es donde se renderiza el componente ProyectoGif */}
-        <ProyectoGif />
-    </React.StrictMode>,
-)
 export const ProyectoGif = () => 
     {
-        return (
-        <>
-        {/* Título */}
-        <h1>ProyectoGif</h1>
-        {/* Input */}
-        {/* listado de gifs */}
-        {/*Gif Item*/}
-        </>
-        )
+        //Usando el Hook useState (función que permite agregar estado de React a los
+        //componentes funcionales)
+        //Para evitar el posible error de Javascript por no definir un valor inicial, se le
+        //pone un arreglo que tenga 'One Punch' como valor inicial
+        const [categories, setCategories] = useState(['One Punch'])
+        return
+
+            
     }
-
-export const ProyectoGif = () => {
-    //Usando el Hook useState (función que permite agregar estado de React a los
-    //componentes funcionales)
-    //Para evitar el posible error de Javascript por no definir un valor inicial, se le
-    //pone un arreglo que tenga 'One Punch' como valor inicial
-    const [categories, setCategories] = useState(['One Punch'])
-    return ()
-
-import {useState} from 'react';
-
+    import { useState} from 'react';
 // Importamos useEffect y useState de React
 import { useEffect, useState } from 'react';
 // Importamos la función getGifs de helpers
 import { getGifs } from '../helpers/getGifs';
-// Definimos un hook personalizado llamado useFetchGifs que toma una categoría como
-//argumento
-
 export const useFetchGifs = (category) => 
     {
+    // creamos una vari
+    // Creamos una variable de estado images y una función para actualizarla setImages
         const [images, setImages] = useState([]);
-        const [isLoading, setIsLoading] = useState(true);
-        const getImages = async () => {
-            const newImages = await getGifs(category);
-            setImages(newImages);
-            setIsLoading(false);
-        }
-        useEffect(() => {
-            getImages();
-        }, []);
-        return {
+        // Creamos una variable de estado isLoading y una función para actualizarla
+        //setIsLoading
+        const [isLoading, setIsLoading] = useState( true );
+        // Definimos una función asíncrona getImages
+        const getImages = async() => {
+        // Obtenemos nuevas imágenes de la categoría con la función getGifs
+        const newImages = await getGifs( category );
+        // Actualizamos images con las nuevas imágenes
+        setImages(newImages);
+        // Establecemos isLoading en falso
+        setIsLoading(false);
+}
+// Usamos useEffect para llamar a getImages cuando se monta el componente
+        useEffect( () => 
+            {
+                getImages();    
+            }, []);
+        // Devolvemos un objeto con las imágenes y el estado de carga
+        return 
+        {
             images,
             isLoading
         }
-    };
-    export const = getGifs = async (category) => {
+    }
